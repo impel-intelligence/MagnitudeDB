dcb7900071ac887d5d91bb41d70ac139ddb45091
Taylor Lineman <taylor.lineman@gmail.com>
2024-04-14 20:13:09 -0400

    Update package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.14.1"),
-        .package(url: "https://github.com/impel-intelligence/SQLite.swift.extensions.git", branch: "main"),
+        .package(url: "https://github.com/impel-intelligence/SQLite.swift.extensions.git", from: "1.0.0"),
         .package(url: "https://github.com/impel-intelligence/SwiftFaiss.git", from: "0.2.0"),
         .package(url: "https://github.com/swiftcsv/SwiftCSV.git", from: "0.8.0"),
         .package(url: "https://github.com/realm/SwiftLint.git", branch: "main")
