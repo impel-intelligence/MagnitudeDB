d21e011bc057a1494593cf95b2e8132df5e0070a
Taylor Lineman <taylor.lineman@gmail.com>
2024-05-08 15:12:51 -0400

    remove swiftlint

--- Package.swift
+++ Package.swift
@@ -17,8 +17,7 @@ let package = Package(
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.14.1"),
         .package(url: "https://github.com/impel-intelligence/SQLite.swift.extensions.git", from: "1.0.0"),
         .package(url: "https://github.com/impel-intelligence/SwiftFaiss.git", from: "0.2.0"),
-        .package(url: "https://github.com/swiftcsv/SwiftCSV.git", from: "0.8.0"),
-        .package(url: "https://github.com/realm/SwiftLint.git", branch: "main")
+        .package(url: "https://github.com/swiftcsv/SwiftCSV.git", from: "0.8.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -29,9 +28,6 @@ let package = Package(
                 .product(name: "SQLite", package: "SQLite.swift"),
                 "SQLite.swift.extensions",
                 "SwiftFaiss"
-            ],
-            plugins: [
-                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")
             ]
         ),
         .testTarget(name: "MagnitudeDBTests", dependencies: ["MagnitudeDB", "SwiftCSV"])
